3ea9c046a89057e4fac49ccd9755bc3e0e718573
Rachit Kumar Tiwari <tiwari.rachit@gmail.com>
2026-09-13 19:26:33 +0530

    Time: 4 ms (36.19%) | Memory: 43.4 MB (40.80%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char i : s.toCharArray()) {
+            if (isBracketOpen(i)) {
+                stack.push(i);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                if (areBracketsMatching(stack.peek(), i)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private boolean isBracketOpen(char c) {
+        switch (c) {
+            case '(':
+            case '{':
+            case '[':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private boolean areBracketsMatching(char c, char d) {
+        if (c == '(' && d == ')')
+            return true;
+        else if (c == '{' && d == '}')
+            return true;
+        else if (c == '[' && d == ']')
+            return true;
+        else
+            return false;
+    }
+}
